df6b2222a57c2d9ec73b401feb24b58112d86953
vamsikrishna <avamsikrishna07@gmail.com>
2024-08-24 16:08:41 +0530

    added a small converstation.sh

---
+++ 02-convestaion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Rakesh : Hi Srinu, How are you ?"
+echo "Srinu : Hello Rakesh . I am fine. How are you doing?"
+echo "Rakesh : I am great Srinu. what about you?"
+echo "Srinu : I started learning shell scripting"
