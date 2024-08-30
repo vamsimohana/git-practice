7155e97d00c04596518f28cf9bf3992414d02fcb
vamsikrishna <avamsikrishna07@gmail.com>
2024-08-30 16:03:23 +0530

    variables

---
+++ 19-delete-old log files.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SOURCE_DIR=/home/ec2-user/logs
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ -d $SOURCE_DIR ]
+then
+    echo -e "$SOURCE_DIR $G Exists $N"
+else
+    echo -e "$SOURCE_DIR $R does not exist $N"
+    exit 1
+fi
+
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+echo "Files: $FILES"
+
+# dont use line, it is reserverd word
+while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+do
+    echo "Deleting file: $file"
+    rm -rf $file
+done <<< $FILES
